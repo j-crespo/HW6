783727273d552331063baaeeee4ec2701d9d9d5a
j-crespo <juliancrespo@cca.edu>
2017-10-16 02:13:56 -0700

    Create stella3.js

---
+++ stella3.js
@@ -0,0 +1,19 @@
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+function draw() { 
+  background(220);
+	
+  
+	fill(199,198,194);
+	rect (0,0,200,200);
+  
+  fill(254,234,99);
+	rect (200,0,200,200);
+  
+  fill(225,65,111);
+	rect (0,200,200,200);
+  
+  fill(7,177,228);
+	rect (200,200,200,200);
